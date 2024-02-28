851e668789acfc24d3ccb1d31a8a1b9988841aab
RagingFire2007 <97838017+RagingFire2007@users.noreply.github.com>
2024-02-28 20:31:58 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Meh)
 })
+input.onButtonPressed(Button.AB, function () {
+    led.plot(0, 0)
+})
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Tortoise)
 })
